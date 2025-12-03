af6f15c4176db1dbd98d90758e7fd717b3ff2cb0
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2025-12-03 23:20:56 +0000

    Actually consume a very small puzzle input. Currently on L or R I'm only moving 1 notch, but the display is counting the number of times zero is landed on correctly. Starting position fudged to 2 instead of 50 in order to test the logic. Next I need to move the correct number of notches

---
+++ day1/atari/includes/input.h
@@ -0,0 +1,30 @@
+  MAC PUZZLE_INPUT
+    ; The puzzle input should be written into this macro
+    ; Each line needs to become the 2 lines:
+    ;    dc.b "<LINE>"
+    ;    byte #0
+    ; The very end of the puzzle input should have two byte #0 lines
+    ;
+    ; The input specified below is from the problem text, not a full puzzle input
+    dc.b "L68"
+    byte #0
+    dc.b "L30"
+    byte #0
+    dc.b "R48"
+    byte #0
+    dc.b "L5"
+    byte #0
+    dc.b "R60"
+    byte #0
+    dc.b "L55"
+    byte #0
+    dc.b "L1"
+    byte #0
+    dc.b "L99"
+    byte #0
+    dc.b "R14"
+    byte #0
+    dc.b "L82"
+    byte #0
+    byte #0
+  ENDM
